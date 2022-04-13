2c3a502df4c5c3b41bf2b39d2e39daea0590f890
unknown <maramzain01@example.com>
2022-04-14 02:46:08 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'dart:developer';
+
+import 'package:flutter/material.dart';
+import 'package:state_managment/todo_app/pages/todo_home_page.dart';
+
+void main() {
+  runApp(MaterialApp(theme: ThemeData.light(), home: TodoHomePage()));
+}
---
+++ lib/todo_app/widgets/task_widget.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:state_managment/todo_app/models/task_model.dart';
+
+class TaskWidget extends StatelessWidget {
+  Function fun;
+  Function delete;
+  Task task;
+  TaskWidget(this.task, this.fun, this.delete);
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Container(
+      margin: EdgeInsets.all(5),
+      decoration: BoxDecoration(
+          color: task.isCompleted
+              ? Color.fromARGB(255, 76, 160, 175)
+              : Color.fromARGB(115, 147, 137, 137),
+          borderRadius: BorderRadius.circular(15)),
+      padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 8),
+      child: Column(children: [
+        CheckboxListTile(
+          title: Text(task.title),
+          value: task.isCompleted,
+          onChanged: (v) {
+            fun(task);
+            delete(task);
+          },
+        ),
+        SizedBox(
+          width: 15,
+        ),
+        CheckboxListTile(
+          title: Text("Delete The Task I am not to do task"),
+          value: task.remove,
+          onChanged: (v) {
+            fun(task);
+            delete(task);
+          },
+        ),
+      ]),
+    );
+  }
+}
